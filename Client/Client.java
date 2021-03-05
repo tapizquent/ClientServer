13d0ba5921a15a6a05960ff388b0b6fcc7224e3f
Jose Tapizquent <josetapizq@gmail.com>
2021-03-05 15:00:17 -0500

    Added flag to allow Client to upload files

--- Client/Client.java
+++ Client/Client.java
@@ -1,4 +1,3 @@
-
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
@@ -13,18 +12,38 @@ public class Client {
     private int bytes;
     private byte[] buffer = new byte[BUFFER_SIZE];
     private File fileToWrite;
+    private static String filename = "";
+    private static ClientMode clientMode = ClientMode.read;
 
     public Client(String host, int port, String filename) {
         try {
-
             connection = new Socket(host, port);
 
             socketIn = new DataInputStream(connection.getInputStream()); // Read data from server
             socketOut = new DataOutputStream(connection.getOutputStream()); // Write data to server
 
-            socketOut.writeUTF(filename); // Write filename to server
+            if (clientMode == ClientMode.write) {
+                // Write flag to let server know whether to upload or download
+                socketOut.writeInt(1);
+                socketOut.writeUTF(filename); // Write filename to server
+                fileToWrite = new File("Files/" + filename);
+                processUpload();
+            } else {
+                // Write flag to let server know whether to upload or download
+                socketOut.writeInt(0);
+                socketOut.writeUTF(filename); // Write filename to server
+                fileToWrite = new File("Files/" + filename);
+                processDownload();
+            }
 
-            fileToWrite = new File("Files/" + filename);
+            connection.close();
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex);
+        }
+    }
+
+    public void processDownload() {
+        try {
             OutputStream os = new FileOutputStream(fileToWrite);
 
             // Read file contents from server
@@ -38,13 +57,104 @@ public class Client {
             }
 
             os.close();
-            connection.close();
-        } catch (Exception ex) {
-            System.out.println("Error: " + ex);
+        } catch (Exception e) {
+            System.out.println("ERROR: Could not read file");
+        }
+    }
+
+    public void processUpload() {
+        try {
+            FileInputStream fileInputStream = new FileInputStream("Files/" + filename);
+            int totalFileByteSize = fileInputStream.available();
+            int bufferSize = totalFileByteSize > BUFFER_SIZE ? BUFFER_SIZE : totalFileByteSize;
+            buffer = new byte[bufferSize];
+
+            while (true) {
+                bytes = fileInputStream.read(buffer, 0, bufferSize); // Read from file
+                if (bytes <= 0)
+                    break; // Check for end of file
+                socketOut.write(buffer, 0, bytes); // Write bytes to socket
+                // printTransferProgress(totalFileByteSize, fileInputStream.available());
+            }
+
+            fileInputStream.close();
+        } catch (Exception e) {
+            System.out.println("ERROR: Could not read file");
         }
     }
 
     public static void main(String[] args) {
-        Client client = new Client("127.0.0.1", 5000, args[0]);
+        try {
+            parseCommandLineArguments(args);
+            Client client = new Client("127.0.0.1", 5000, filename);
+        } catch (InvalidArgumentException e) {
+            printConsoleHelp(e.getCode());
+        }
     }
+
+    public static void parseCommandLineArguments(String[] args) throws InvalidArgumentException {
+        if (args.length <= 0) {
+            throw new InvalidArgumentException(ArgumentErrorCode.missingAll);
+        }
+
+        // We are trying to access Client in Write mode
+        if (args[0].compareTo("-w") == 0) {
+            if (args.length < 2) {
+                throw new InvalidArgumentException(ArgumentErrorCode.filename);
+            }
+
+            filename = args[1];
+            clientMode = ClientMode.write;
+        } else {
+            filename = args[0];
+        }
+    }
+
+    public static void printConsoleHelp(ArgumentErrorCode code) {
+        switch (code) {
+            case filename:
+                System.out.println("ERROR: FILE NAME MISSING");
+                break;
+            case missingAll:
+                System.out.println("ERROR: MISSING ARGUMENTS");
+                break;
+            case startByte:
+                System.out.println("ERROR: START BYTE VALUE MISSING");
+                break;
+            default:
+                System.out.println("ERROR: INVALID ARGUMENT");
+                break;
+        }
+
+        System.out.println();
+        System.out.println("Please use as follows:");
+        System.out.println();
+        System.out.println("  java Client [-w] <filename> [-s <startByte>] [-e <endByte>]");
+        System.out.println();
+        System.out.println("  <filename> : Required whether reading or writting file");
+        System.out.println("  -w : Sets Client in write mode. File with <filename> will upload to Server");
+        System.out.println("  -s : Sets the start byte to copy file (if whole file is not needed)");
+        System.out.println("  -e : Sets the end byte to copy file (if whole file is not needed)");
+    }
+}
+
+class InvalidArgumentException extends Exception {
+    private ArgumentErrorCode code;
+    private static final long serialVersionUID = 1L;
+
+    public InvalidArgumentException(ArgumentErrorCode code) {
+        this.code = code;
+    }
+
+    public ArgumentErrorCode getCode() {
+        return code;
+    }
+}
+
+enum ArgumentErrorCode {
+    filename, startByte, missingAll
+}
+
+enum ClientMode {
+    read, write
 }
--- Server/Server.java
+++ Server/Server.java
@@ -1,7 +1,4 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,11 +9,15 @@ public class Server {
     private DataInputStream socketIn;
     private DataOutputStream socketOut;
     private FileInputStream fileIn;
+    private FileOutputStream fileOut;
     private String filename;
     private int bytes;
     private byte[] buffer = new byte[BUFFER_SIZE];
     private int debugFlag = 0;
     private long percentageTransferred = 0;
+    // Client mode 0 = READ
+    // Client mode 1 = WRITE
+    private int clientMode = 0;
 
     public Server(int port, int debugFlag) {
         this.debugFlag = debugFlag;
@@ -30,26 +31,23 @@ public class Server {
                     socketIn = new DataInputStream(connection.getInputStream()); // Read data from client
                     socketOut = new DataOutputStream(connection.getOutputStream()); // Write data to client
 
+                    clientMode = socketIn.readInt(); // Read clientMode from Client
                     filename = socketIn.readUTF(); // Read filename from client
 
-                    fileIn = new FileInputStream("Files/" + filename);
-
                     if (debugFlag == 1) {
-                        System.out.println("Sending " + filename + " to " + connection.getInetAddress());
+                        if (clientMode == 0) {
+                            System.out.println("Sending " + filename + " to " + connection.getInetAddress());
+                        } else {
+                            System.out.println("Receiving " + filename + " from " + connection.getInetAddress());
+                        }
                     }
 
-                    int totalFileByteSize = fileIn.available();
-                    int bufferSize = totalFileByteSize > BUFFER_SIZE ? BUFFER_SIZE : totalFileByteSize;
-                    buffer = new byte[bufferSize];
-
-                    // Write file contents to client
-                    while (true) {
-                        bytes = fileIn.read(buffer, 0, bufferSize); // Read from file
-                        if (bytes <= 0)
-                            break; // Check for end of file
-                        socketOut.write(buffer, 0, bytes); // Write bytes to socket
-                        printTransferProgress(totalFileByteSize, fileIn.available());
+                    if (clientMode == 0) {
+                        processClientDownload();
+                    } else {
+                        processClientUpload();
                     }
+
                 } catch (Exception ex) {
                     System.out.println("Error: " + ex);
                     System.out.println("filename: " + filename);
@@ -59,14 +57,15 @@ public class Server {
                         connection.close();
                     }
 
-                    if (fileIn != null) {
-                        fileIn.close();
-                    }
-
                     percentageTransferred = 0;
 
                     if (debugFlag == 1) {
-                        System.out.println("Finished sending " + filename + " to " + connection.getInetAddress());
+                        if (clientMode == 0) {
+                            System.out.println("Finished sending " + filename + " to " + connection.getInetAddress());
+                        } else {
+                            System.out
+                                    .println("Finished receiving " + filename + " from " + connection.getInetAddress());
+                        }
                     }
 
                     System.out.println();
@@ -77,8 +76,43 @@ public class Server {
         }
     }
 
-    // if bytesLeft == 0
-    // 38 X
+    private void processClientDownload() throws IOException, FileNotFoundException {
+        fileIn = new FileInputStream("Files/" + filename);
+        int totalFileByteSize = fileIn.available();
+        int bufferSize = totalFileByteSize > BUFFER_SIZE ? BUFFER_SIZE : totalFileByteSize;
+        buffer = new byte[bufferSize];
+
+        while (true) {
+            bytes = fileIn.read(buffer, 0, bufferSize); // Read from file
+            if (bytes <= 0)
+                break; // Check for end of file
+            socketOut.write(buffer, 0, bytes); // Write bytes to socket
+            printTransferProgress(totalFileByteSize, fileIn.available());
+        }
+
+        fileIn.close();
+    }
+
+    private void processClientUpload() throws FileNotFoundException, IOException {
+        try {
+            File fileToWrite = new File("Files/" + filename);
+            fileOut = new FileOutputStream(fileToWrite);
+
+            // Read file contents from server
+            while (true) {
+                bytes = socketIn.read(buffer, 0, BUFFER_SIZE); // Read from socket
+                if (bytes <= 0)
+                    break; // Check for end of file
+
+                // Write
+                fileOut.write(buffer, 0, bytes);
+            }
+
+            fileOut.close();
+        } catch (Exception e) {
+            System.out.println("ERROR: Could not read file");
+        }
+    }
 
     private void printTransferProgress(long totalBytes, long bytesLeft) {
         if (debugFlag == 0)
